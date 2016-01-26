606e57f92b6352ab2ddee3fec50b0cb76c3daba2
Marina Borsodi-Benson <marinaborsodibenson@gmail.com>
2016-01-26 12:01:36 -0500

    New: added offset indexer

---
+++ indexers/offset_indexer.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Program Name:           vis-rodan
+# Program Description:    Job wrappers that allows vis-framework to work in Rodan.
+#
+# Filename:               vis-rodan/indexers/noterest_indexer.py
+# Purpose:                Wrapper for NoteRest Indexer.
+#
+# Copyright (C) 2015 DDMAL
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#--------------------------------------------------------------------------------------------------
+import numpy
+from pandas import concat, DataFrame
+from rodan.jobs.base import RodanTask
+
+import logging
+logger = logging.getLogger('rodan')
+
+class VROffsetIndexer(RodanTask):
+
+	name = 'vis-rodan.indexer.VF_offset_indexer'
+	author = 'Marina Borsodi-Benson'
+	description = 'Filter by offset'
+	settings = {
+		'title': 'Offset Indexer Settings',
+		'type': 'object',
+		'properties': {
+			'Quarternote length': {
+				'type': 'float',
+				'default': 1.0
+			},
+			'Method': {
+				'enum': ['ffill', 'None'],
+				'type': 'string',
+				'default': 'ffill'
+			}
+		}
+	}
+
+	enabled = True
+	category = 'Indexer'
+	interactive = False
+
+	input_port_types = [{
+		'name': 'NoteRest Indexer Result',
+		'resource_types': ['application/x-vis_noterest_pandas_series+csv'],
+		'minimum': 1,
+		'maximum': 1,
+	}]
+	output_port_types = [{
+		'name': 'Offset Indexer Result',
+		'resource_types': ['application/x-vis_noterest_pandas_series+csv'],
+		'minimum': 1,
+		'maximum': 1
+	}]
+
+	def run_my_task(self, inputs, settings, outputs):
+
+		infile = inputs['NoteRest Indexer Result'][0]['resource_path']
+		outfile = outputs['Offset Indexer Result'][0]['resource_path']
+
+		wrapper_settings = dict([(k, settings[k]) for k in ('Quarternote length', 'Method')])
+		execution_settings = dict()
+		execution_settings['quarterLength'] = wrapper_settings['Quarternote length']
+		if wrapper_settings['Method'] == 0:
+			execution_settings['method'] = 'ffill'
+		else:
+			execution_settings['method'] = 'None'
+		execution_settings['mp'] = False
+
+		noterest = DataFrame.from_csv(noterest_file, header = [0, 1])
+		offset_indexed = OffsetIndexer(noterest, execution_settings).run()
+		offset_indexed.to_csv(outfile)
+
+		return True
